753f91f5cc848cdc55a0dda963a577e76418cd3d
Hanzzi <hans-byager@live.dk>
2017-09-06 14:57:06 +0200

    Add project files.

---
+++ Ludo Object Oriented/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ludo_Object_Oriented
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
